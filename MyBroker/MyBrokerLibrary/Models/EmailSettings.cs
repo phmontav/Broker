d7ab9f7ddf2961e8cb75f34f71b65e1907396aa3
phmontav <phmontav@gmail.com>
2023-04-06 23:48:16 -0300

    library project added

---
+++ MyBroker/MyBrokerLibrary/Models/EmailSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBrokerLibrary.Models
+{
+    public class EmailSettings
+    {
+        public string? fromAddress { get; set; }
+        public List<string>? toEmail { get; set; }
+        public string? subject { get; set; }
+        public string? plainTextContent { get; set; }
+        public string? htmlContent { get; set; }
+    }
+}
+
---
+++ MyBroker/MyBrokerLibrary/Services/EmailService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace MyBroker
+{
+    public class EmailService
+    {
+        private readonly IConfigurationRoot _config;
+
+        EmailService(IConfigurationRoot config)
+        {
+            _config = config;
+        }
+        static async Task sendEmail()
+        {
+            IConfigurationSection options = _config.GetSection("SMTPSettings");
+            var apiKey = _config.GetSection("ConnectionString")["SMTPApiKey"];
+            var client = new SendGridClient(apiKey);
+
+            var from = new EmailAddress(options["fromAddress"],"example user");
+            var to = new EmailAddress(options["toEmail"], "Example User");
+            var subject = options["subject"];
+            var plainTextContent = options["plainTextContent"];
+            var htmlContent = options["htmlContent"];
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            var response = await client.SendEmailAsync(msg);
+            Console.WriteLine(response.StatusCode);
+
+        }
+    }
+}
