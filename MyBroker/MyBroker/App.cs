13d2b00e93c59f6e9554be57c79d142f5a9ba1e2
phmontav <phmontav@gmail.com>
2023-04-07 11:44:14 -0300

    getting stock price and adding some DI

---
+++ MyBroker/MyBroker/App.cs
@@ -0,0 +1,25 @@
+﻿using MyBrokerLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBroker
+{
+    internal class App
+    {
+        private readonly IStockDataService stockDataService;
+
+        public App(IStockDataService stockDataService)
+        {
+            this.stockDataService = stockDataService;
+        }
+
+        public async Task Run(string[] args)
+        {
+            var aux = await this.stockDataService.getStockPrice("PETR4.SA");
+           Console.WriteLine(aux);
+        }
+    }
+}
